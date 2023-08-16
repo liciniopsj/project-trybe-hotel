57ca0992c3b0f12e4993e2d5fa0cb742faa8124b
liciniopsj <licinio.psj@gmail.com>
2023-08-16 15:18:33 -0300

    HotelRepo - Addhotel, Gethotel

--- src/TrybeHotel/Repository/HotelRepository.cs
+++ src/TrybeHotel/Repository/HotelRepository.cs
@@ -1,5 +1,6 @@
 using TrybeHotel.Models;
 using TrybeHotel.Dto;
+using Microsoft.EntityFrameworkCore;
 
 namespace TrybeHotel.Repository
 {
@@ -14,13 +15,43 @@ namespace TrybeHotel.Repository
         // 4. Desenvolva o endpoint GET /hotel
         public IEnumerable<HotelDto> GetHotels()
         {
-            throw new NotImplementedException();
+            var hotels = _context.Hotels
+                .Include(hotel => hotel.City)
+                .Select(hotel => new HotelDto
+                {
+                    HotelId = hotel.HotelId,
+                    Name = hotel.Name,
+                    Address = hotel.Address,
+                    CityId = hotel.CityId,
+                    CityName = hotel.City.Name
+                });
+
+            return hotels.ToList();
         }
-        
+
         // 5. Desenvolva o endpoint POST /hotel
         public HotelDto AddHotel(Hotel hotel)
         {
-            throw new NotImplementedException();
+            var city = _context.Cities.First(c => c.CityId == hotel.CityId);
+
+            var newHotel = new Hotel
+            {
+                Name = hotel.Name,
+                Address = hotel.Address,
+                CityId = hotel.CityId
+            };
+
+            _context.Hotels.Add(newHotel);
+            _context.SaveChanges();
+
+            return new HotelDto
+            {
+                HotelId = newHotel.HotelId,
+                Name = newHotel.Name,
+                Address = newHotel.Address,
+                CityId = newHotel.CityId,
+                CityName = city.Name
+            };
         }
     }
 }
