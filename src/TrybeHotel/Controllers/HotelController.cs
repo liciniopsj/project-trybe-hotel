9fd5b5cc6d66c9188f01092d53bd901b33e212d6
liciniopsj <licinio.psj@gmail.com>
2023-08-16 14:55:02 -0300

    Hotel Controller

--- src/TrybeHotel/Controllers/HotelController.cs
+++ src/TrybeHotel/Controllers/HotelController.cs
@@ -15,19 +15,20 @@ namespace TrybeHotel.Controllers
         {
             _repository = repository;
         }
-        
+
         // 4. Desenvolva o endpoint GET /hotel
         [HttpGet]
-        public IActionResult GetHotels(){
-            throw new NotImplementedException();
+        public IActionResult GetHotels()
+        {
+            return Ok(_repository.GetHotels());
         }
 
         // 5. Desenvolva o endpoint POST /hotel
         [HttpPost]
-        public IActionResult PostHotel([FromBody] Hotel hotel){
-            throw new NotImplementedException();
+        public IActionResult PostHotel([FromBody] Hotel hotel)
+        {
+            return Created("", _repository.AddHotel(hotel));
         }
-
 
     }
 }
