72dba8397f94280558480372f39c0baad875e7a2
liciniopsj <licinio.psj@gmail.com>
2023-08-16 18:12:52 -0300

    GetRooms

--- src/TrybeHotel/Repository/RoomRepository.cs
+++ src/TrybeHotel/Repository/RoomRepository.cs
@@ -12,18 +12,39 @@ namespace TrybeHotel.Repository
         }
 
         // 6. Desenvolva o endpoint GET /room/:hotelId
-        public IEnumerable<RoomDto> GetRooms(int HotelId)
+        public IEnumerable<RoomDto> GetRooms(int hotelId)
         {
-            throw new NotImplementedException(); 
+            var roomsDto = _context.Rooms
+                .Where(room => room.HotelId == hotelId)
+                .Select(room => new RoomDto
+                {
+                    RoomId = room.RoomId,
+                    Name = room.Name,
+                    Capacity = room.Capacity,
+                    Image = room.Image,
+                    Hotel = new HotelDto
+                    {
+                        HotelId = room.Hotel.HotelId,
+                        Name = room.Hotel.Name,
+                        Address = room.Hotel.Address,
+                        CityId = room.Hotel.CityId,
+                        CityName = room.Hotel.City.Name
+                    }
+                });
+
+            return roomsDto.ToList();
         }
 
+
         // 7. Desenvolva o endpoint POST /room
-        public RoomDto AddRoom(Room room) {
-            throw new NotImplementedException(); 
+        public RoomDto AddRoom(Room room)
+        {
+            throw new NotImplementedException();
         }
 
         // 8. Desenvolva o endpoint DELETE /room/:roomId
-        public void DeleteRoom(int RoomId) {
+        public void DeleteRoom(int RoomId)
+        {
             throw new NotImplementedException();
         }
     }
